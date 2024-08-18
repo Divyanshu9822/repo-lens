4c5e0c5e141705bbdbcde83b9fdb61c4251442e2
Divyanshu Prasad <divyanshu9871@gmail.com>
2024-08-18 14:00:29 +0530

    added redirect uri key in env

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
 GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
 
 # OAuth redirect URI
-REDIRECT_URI = "http://localhost:8501/"
+REDIRECT_URI = os.getenv("REDIRECT_URI")
 
 # Create an OAuth2 session
 oauth = OAuth2Session(
